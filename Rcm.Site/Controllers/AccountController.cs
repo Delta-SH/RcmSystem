4b8365a5b758fc32e32e121277a9f4b0627d71cc
steven <huangying0621@gmail.com>
2018-10-17 14:55:30 +0800

    优化登录页面，增加OAuth登录接口

--- Rcm.Site/Controllers/AccountController.cs
+++ Rcm.Site/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using Rcm.Core.Enum;
 using Rcm.Service;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -41,6 +42,93 @@ namespace Rcm.Site.Controllers {
             Request.Cookies.Clear();
             FormsAuthentication.SignOut();
             return RedirectToRoute("HomePage");
+        }
+
+        public ActionResult iLogin(string returnUrl) {
+            try {
+                var uid = ConfigurationManager.AppSettings["OUid"];
+                if (String.IsNullOrWhiteSpace(uid))
+                    ModelState.AddModelError("", "接口用户名未配置。");
+
+                var password = ConfigurationManager.AppSettings["OPwd"];
+                if (ModelState.IsValid && String.IsNullOrWhiteSpace(password))
+                    ModelState.AddModelError("", "接口密码未配置。");
+
+                if (ModelState.IsValid) {
+                    var loginResult = _userService.Validate(uid, password);
+                    switch (loginResult) {
+                        case LoginResult.Successful:
+                        break;
+                        case LoginResult.NotExist:
+                        ModelState.AddModelError("", "用户名不存在。");
+                        break;
+                        case LoginResult.NotEnabled:
+                        ModelState.AddModelError("", "用户已禁用，请与管理员联系。");
+                        break;
+                        case LoginResult.Expired:
+                        ModelState.AddModelError("", "用户已过期，请与管理员联系。");
+                        break;
+                        case LoginResult.WrongPassword:
+                        default:
+                        ModelState.AddModelError("", "密码错误，登录失败。");
+                        break;
+                    }
+                }
+
+                if (ModelState.IsValid) {
+                    var current = _userService.GetUser(uid);
+                    var loginResult = _roleService.Validate(current.GroupId);
+                    switch (loginResult) {
+                        case LoginResult.Successful:
+                        break;
+                        case LoginResult.RoleNotExist:
+                        ModelState.AddModelError("", "角色不存在。");
+                        break;
+                        case LoginResult.RoleNotEnabled:
+                        ModelState.AddModelError("", "角色已禁用，请与管理员联系。");
+                        break;
+                        default:
+                        ModelState.AddModelError("", "角色错误。");
+                        break;
+                    }
+                }
+
+                if (ModelState.IsValid) {
+                    var current = _userService.GetUser(uid);
+                    var now = DateTime.Now;
+                    var ticket = new FormsAuthenticationTicket(
+                        1,
+                        uid,
+                        now,
+                        now.Add(FormsAuthentication.Timeout),
+                        false,
+                        current.LastId == 0 ? "10078" : current.GroupId.ToString(),
+                        FormsAuthentication.FormsCookiePath);
+
+                    var encryptedTicket = FormsAuthentication.Encrypt(ticket);
+                    var authCookie = new HttpCookie(FormsAuthentication.FormsCookieName, encryptedTicket);
+                    authCookie.HttpOnly = true;
+                    authCookie.Path = FormsAuthentication.FormsCookiePath;
+
+                    if (ticket.IsPersistent) {
+                        authCookie.Expires = ticket.Expiration;
+                    }
+
+                    if (FormsAuthentication.CookieDomain != null) {
+                        authCookie.Domain = FormsAuthentication.CookieDomain;
+                    }
+
+                    Response.Cookies.Add(authCookie);
+                    if (Url.IsLocalUrl(returnUrl))
+                        return Redirect(returnUrl);
+                    else
+                        return RedirectToRoute("HomePage");
+                }
+            } catch (Exception exc) {
+                ModelState.AddModelError("", exc.Message);
+            }
+
+            return View();
         }
 
         public ActionResult GetCaptcha() {
