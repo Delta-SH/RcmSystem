074406d0b3def69dcf4676c95f6fe1f9f0fd8e02
steven <huangying0621@gmail.com>
2019-07-19 09:51:49 +0800

    修改组态上线提示

--- Rcm.Site/Scripts/home.configure.js
+++ Rcm.Site/Scripts/home.configure.js
@@ -1,7 +1,21 @@
 ﻿(function () {
     Ext.onReady(function () {
         var hcontent = Ext.create('Ext.panel.Panel', {
-            region: 'center'
+            region: 'center',
+            layout: {
+                type: 'hbox',
+                padding: '5',
+                pack: 'center',
+                align: 'middle'
+            },
+            border: false,
+            items: [
+                {
+                    xtype: 'box',
+                    cls:'alert alert-info',
+                    html: '<span class="iconfont icon-tips" aria-hidden="true"></span> 即将上线，敬请期待！'
+                }
+            ]
         });
 
         /*add components to viewport panel*/
